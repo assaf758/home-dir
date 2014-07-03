2f9cae83f376c228a03d8ff15a8c245f35f28de0
assaf758 <assaf758@hlinux.hlinux>
2014-07-03 11:53:47 +0300

    varios changes

--- scripts/kbd_home.sh
+++ scripts/kbd_home.sh
@@ -2,6 +2,7 @@
 #The caps-lock key serves as additional ctrl. no caps-lock functionality
 setxkbmap -rules evdev -model logitech_base -layout 'us,il' -variant '' -option  
 setxkbmap -rules evdev -model logitech_base -layout 'us,il' -variant '' -option 'ctrl:nocaps', 'grp:shifts_toggle'
+xmodmap -e "clear lock"
 setxkbmap -query
 # note that xmodmap seems to break the "esc" funciotnality in slickedit
 
